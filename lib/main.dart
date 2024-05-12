1d9454b2076399dcf6feb688ba3051d679bb630e
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-05-12 16:26:49 +0800

    add pages and nav route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_bloc/pages/edit_page.dart';
 import 'package:flutter_todo_bloc/pages/home_page.dart';
+import 'package:flutter_todo_bloc/pages/settings_page.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      // home: const HomePage(),
+      routerConfig: GoRouter(routes: [
+        GoRoute(path: "/", builder: (_, __) => const HomePage()),
+        GoRoute(path: "/edit", builder: (_, __) => const EditPage()),
+        GoRoute(path: "/settings", builder: (_, __) => const SettingsPage()),
+      ]),
     );
   }
 }
