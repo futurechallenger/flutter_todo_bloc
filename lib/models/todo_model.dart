ec1d5a8f70ce5eb3adedcb5a2bec6ab018bab6ea
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-05-14 15:42:57 +0800

    ignore auto gen files

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,1 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'todo_model.freezed.dart';
+part 'todo_model.g.dart';
+
+@freezed
+class TodoItem with _$TodoItem {
+  const factory TodoItem({
+    int? id,
+    required String content,
+    String? note,
+    int? deleted,
+    int? status,
+  }) = _TodoItem;
+
+  factory TodoItem.fromJson(Map<String, dynamic> json) =>
+      _$TodoItemFromJson(json);
+}
