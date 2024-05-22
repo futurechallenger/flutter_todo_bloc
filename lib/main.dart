ddd851914a29b6f60ae929b1ccb2ad799a81fd6e
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-05-22 18:10:28 +0800

    refactor edit-page and related cubit / state / router

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_todo_bloc/bloc/home_bloc.dart';
 import 'package:flutter_todo_bloc/bloc/todo_detail_cubit.dart';
+import 'package:flutter_todo_bloc/models/todo_model.dart';
 import 'package:flutter_todo_bloc/pages/edit_page.dart';
 import 'package:flutter_todo_bloc/pages/home_page.dart';
 import 'package:flutter_todo_bloc/pages/settings_page.dart';
@@ -12,7 +13,8 @@ void main() {
     BlocProvider<TodoListBloc>(
         create: (BuildContext context) =>
             TodoListBloc()..add(TodoListRequested())),
-    BlocProvider<TodoDetailCubit>(create: (_) => TodoDetailCubit()),
+    // BlocProvider<TodoDetailCubit>(
+    //     create: (_) => TodoDetailCubit(const TodoDetailState())),
   ], child: const MyApp()));
 }
 
@@ -30,7 +32,14 @@ class MyApp extends StatelessWidget {
       // home: const HomePage(),
       routerConfig: GoRouter(routes: [
         GoRoute(path: "/", builder: (_, __) => const HomePage()),
-        GoRoute(path: "/edit", builder: (_, __) => const EditPage()),
+        GoRoute(
+            path: "/edit",
+            builder: (context, state) {
+              return BlocProvider(
+                  create: (context) =>
+                      TodoDetailCubit(todo: state.extra as TodoItem),
+                  child: const EditPage());
+            }),
         GoRoute(path: "/settings", builder: (_, __) => const SettingsPage()),
       ]),
     );
--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -3,20 +3,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_todo_bloc/bloc/todo_detail_cubit.dart';
 
 class EditPage extends StatelessWidget {
-  const EditPage({super.key});
+  const EditPage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final todo =
+        context.select((TodoDetailCubit cubit) => cubit.state.todoItem);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editing'),
       ),
-      body: BlocBuilder<TodoDetailCubit, TodoDetailState>(
-        builder: (context, state) {
-          return Center(
-            child: Text(state.todoItem?.content ?? ""),
-          );
-        },
+      body: Center(
+        child: Text(todo?.content ?? ""),
       ),
     );
   }
